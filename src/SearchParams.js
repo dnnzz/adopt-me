78ea656c702861b8fc0d467a5790d56bd877ff7f
dnnzz <l1611012074@stud.sdu.edu.tr>
2021-02-05 02:10:04 +0300

    --

---
+++ src/SearchParams.js
@@ -0,0 +1,66 @@
+import React , {useState , useEffect , useContext} from "react";
+import pet , {ANIMALS} from "@frontendmasters/pet";
+import useDropdown from "./useDropdown";
+import ThemeContext from "./ThemeContext";
+import Results from "./Results";
+
+const SearchParams = () =>{
+    // right side useState is default state ! 
+    const [location, updateLocation] = useState("Seattle, WA");
+    const [breeds,updateBreeds] = useState([]);
+    const [animal,AnimalDropdown] = useDropdown("Animal","dog",ANIMALS);
+    const [breed,BreedDropdown, updateBreed] = useDropdown("Breed","",breeds);
+    const [pets, setPets] = useState([]);
+    const [theme,setTheme] = useContext(ThemeContext);
+
+    async function requestPets(){
+        const {animals} = await pet.animals({
+            location,
+            breed,
+            type:animal
+        })
+        setPets(animals || []);
+    }
+    useEffect(() =>{
+        updateBreeds([]);
+        updateBreed("");
+        pet.breeds(animal).then(({breeds})=>{
+            const breedStrings  = breeds.map(({name})=> name);
+            updateBreeds(breedStrings);
+        }, error => console.error(error));
+    },[animal,updateBreed,updateBreeds]);
+
+    return(
+        <div className="search-params">
+            <form onSubmit = {(e) =>{
+                e.preventDefault();
+                requestPets();
+            } }>
+                <label htmlFor="location">
+                    Location
+                    <input id="location" 
+                    value={location} 
+                    placeholder="Location" 
+                    onChange={e => updateLocation(e.target.value)}/>
+                </label>
+                <AnimalDropdown/>
+                <BreedDropdown/>
+                <label htmlFor="theme">
+                    Theme
+                    <select
+                    value={theme}
+                    onChange={e => setTheme(e.target.value)}
+                    onBlur={e => setTheme(e.target.value)}>
+                        <option value="peru">Peru</option>
+                        <option value="darkblue">Dark Blue</option>
+                        <option value="chartreuse">Chartreuse</option>
+                        <option value="mediumorchid">Medium Orchid</option>
+                    </select>
+                </label>
+                <button style={{backgroundColor:theme}}>Submit</button>
+            </form>
+            <Results pets={pets} />
+        </div>
+    );
+};
+export default SearchParams;
